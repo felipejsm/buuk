2ae84aea42ccca94ebd84cd4452113b12a8e30bc
felipejsm <felipejsm41@gmail.com>
2021-03-18 06:54:40 -0300

    first commit

---
+++ src/main/java/com/buuk/BuukApplication.java
@@ -0,0 +1,13 @@
+package com.buuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuukApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuukApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buuk/BuukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
